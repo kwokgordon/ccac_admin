b8640980378b734630f4c5324e74c7e36cafc8b0
Gordon Kwok <Gordon.Kwok@paysafe.com>
2016-02-18 17:41:23 -0700

    2016-02-18 adding upload pages

--- app/controllers/content.js
+++ app/controllers/content.js
@@ -30,6 +30,12 @@ module.exports = function(app, passport) {
             user : req.user // get the user out of session and pass to template
         });
     });
+
+    app.get('/profile/uploads', shared.isLoggedIn, shared.checkPermission(["uploads"]), function(req, res) {
+        res.render('content/uploads', {
+            user : req.user // get the user out of session and pass to template
+        });
+    });
 	
     app.get('/no_permission', shared.isLoggedIn, function(req, res) {
         res.render('content/no_permission', {
---
+++ public/js/angular/content/uploads.js
@@ -0,0 +1,10 @@
+var ccac = angular.module('ccacAdminApp', ['ui.bootstrap']);
+
+ccac.controller('UploadsController', function ($scope, $http, $log) {
+
+	$scope.formData = {};
+
+	$scope.init = function() {
+	};
+	
+});
